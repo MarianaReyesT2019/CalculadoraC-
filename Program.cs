6f9765aa9a42ddb6bab97fa2d0a6ab13172c7e23
JMarianaStark <marianareyes9@gmail.com>
2021-03-01 20:11:09 -0600

    Commit para subir a Git

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Operaciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cantidad1;
+            double cantidad2;
+            string operacion;
+            double resultado;
+
+
+            Console.WriteLine("Escribe la operacion que desea realizar");
+            Console.WriteLine("suma");
+            Console.WriteLine("resta");
+            Console.WriteLine("multiplicacion");
+            Console.WriteLine("division");
+
+            operacion = Console.ReadLine();
+
+            Console.WriteLine("Escribe la primera cantidad");
+            cantidad1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Escribe la segunda cantidad");
+            cantidad2 = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.Clear();
+
+
+            if(operacion == "suma")
+            {
+                resultado = cantidad1 + cantidad2;
+                Console.WriteLine("El resultado de la suma es: " + resultado);
+                Console.ReadLine();
+
+            }
+
+            if(operacion  == "resta")
+            {
+                resultado = cantidad1 - cantidad2;
+                Console.WriteLine("El resultado de la resta es: " + resultado);
+                Console.ReadLine();
+
+            }
+
+            if( operacion == "multiplicacion")
+            {
+                resultado = cantidad1 * cantidad2;
+                Console.WriteLine("El resultado de la multiplicaion es: " + resultado);
+                Console.ReadLine();
+
+            }
+
+            if (operacion == "division ")
+            {
+                resultado = cantidad1 / cantidad2;
+                Console.WriteLine(" El resultado de la division es: " + resultado);
+                Console.ReadLine();
+
+            }
+            
+        }
+    }
+}
